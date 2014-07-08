d01393272d4db6263143695e53ed7c85e3e9e4ff
Edward Guo <edwardplguo@gmail.com>
2014-07-08 19:50:29 -0400

    Remove ECTASubScrollView and Add ECTAGateInfoView

---
+++ Shared/ECTAGateInfoView.h
@@ -0,0 +1,13 @@
+//
+//  ECTAGateInfoView.h
+//  Circuit
+//
+//  Created by Edward Guo on 2014-07-08.
+//  Copyright (c) 2014 Edward Peiliang Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECTAGateInfoView : UIView
+
+@end
--- Shared/ECTAGateInfoView.m
+++ Shared/ECTAGateInfoView.m
@@ -1,14 +1,14 @@
 //
-//  ECTASubScrollView.m
+//  ECTAGateInfoView.m
 //  Circuit
 //
 //  Created by Edward Guo on 2014-07-08.
 //  Copyright (c) 2014 Edward Peiliang Guo. All rights reserved.
 //
 
-#import "ECTASubScrollView.h"
+#import "ECTAGateInfoView.h"
 
-@implementation ECTASubScrollView
+@implementation ECTAGateInfoView
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -18,14 +18,5 @@
     }
     return self;
 }
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
 
 @end
